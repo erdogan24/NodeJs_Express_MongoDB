617062a1fbe198e32b4f3c0336a19e0df4d9facb
erdoganakpolat <erdoganakpolat98@gmail.com>
2025-11-29 21:39:39 +0300

    new update

--- app.js
+++ app.js
@@ -3,14 +3,7 @@ const app = express();
 require("dotenv").config();
 require("./src/db/dbConnection");
 const port = process.env.PORT || 5001;
-
-app.get("/", (req, res) => {
-  res.json({
-    message: "Welcome",
-  });
-});
 const router = require("./src/routers");
-app.use("/api", router);
 
 // Middlewares
 app.use(express.json());
@@ -18,6 +11,14 @@ app.use(express.json({ limit: "50mb" }));
 app.use(
   express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 })
 );
+
+app.use("/api", router);
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome",
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server ${port} working from port...`);
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,10 +1,16 @@
-const login = async (requestAnimationFrame, res) => {
+const user = require("../models/user.model");
+
+const login = async (req, res) => {
   console.log(req.body);
 
   return res.json(req.body);
 };
 
-const register = async (requestAnimationFrame, res) => {
+const register = async (req, res) => {
+  const { email } = req.body;
+
+  const userCheck = await user.findOne({ email });
+
   console.log(req.body);
 };
 
