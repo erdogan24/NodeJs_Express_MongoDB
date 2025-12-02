9819eb04cb700a2084f24810c0d7c20f570824e8
erdoganakpolat <erdoganakpolat98@gmail.com>
2025-12-02 20:18:58 +0300

    auth.validation.js has been added.

--- src/middlewares/validations/auth.validation.js
+++ src/middlewares/validations/auth.validation.js
@@ -1,2 +1,41 @@
 const joi = require("joi");
 const APIError = require("../../utils/errors");
+
+class authValidation {
+  constructor() {}
+  static register = async (req, res, next) => {
+    try {
+      await joi.object({
+        name: joi.string().trim().min(3).max(100).required().messages({
+          "string.base": "namespace must be text",
+          "string.empty": "namespace cannot be empty",
+          "string.min": "namespace must be 3 character at least",
+          "string.max": "namespace must be 10 character at most",
+          "string.required": "namespace must be",
+        }),
+        lastName: joi.string().trim().min(3).max(100).required().messages({
+          "string.base": "Surnamespace must be text",
+          "string.empty": "Surnamespace cannot be empty",
+          "string.min": "Surnamespace must be 3 character at least",
+          "string.max": "Surnamespace must be 10 character at most",
+          "string.required": "namespace must be",
+        }),
+        email: joi.string().email().trim().min(3).max(100).required().messages({
+          "string.base": "Email must be text",
+          "string.empty": "Email cannot be empty",
+          "string.min": "Email must be 3 character at least",
+          "string.email": "Please sign in your email",
+          "string.max": "Email must be 10 character at most",
+          "string.required": "Emailmust be",
+        }),
+        password: joi.string().trim().min(6).max(36).required().messages({
+          "string.base": "Password must be text",
+          "string.empty": "Password cannot be empty",
+          "string.min": "password must be 6 character at least",
+          "string.max": "Password must be 36 character at most",
+          "string.required": "Password must be",
+        }),
+      });
+    } catch (error) {}
+  };
+}
