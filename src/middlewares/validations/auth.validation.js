009758dad45099145712ab278e0aeae5f520de5f
erdoganakpolat <erdoganakpolat98@gmail.com>
2025-12-03 23:00:59 +0300

    new update 3

--- src/middlewares/validations/auth.validation.js
+++ src/middlewares/validations/auth.validation.js
@@ -55,28 +55,38 @@ class authValidation {
 
   static login = async (req, res, next) => {
     try {
-      await joi.object({
-        email: joi.string().email().trim().min(3).max(100).required().messages({
-          "string.base": "Email must be text",
-          "string.empty": "Email cannot be empty",
-          "string.min": "Email must be 3 character at least",
-          "string.email": "Please sign in your email",
-          "string.max": "Email must be 10 character at most",
-          "string.required": "Emailmust be",
-        }),
-        password: joi.string().trim().min(6).max(36).required().messages({
-          "string.base": "Password must be text",
-          "string.empty": "Password cannot be empty",
-          "string.min": "password must be 6 character at least",
-          "string.max": "Password must be 36 character at most",
-          "string.required": "Password must be",
-        }),
-      });
+      await joi
+        .object({
+          email: joi
+            .string()
+            .email()
+            .trim()
+            .min(3)
+            .max(100)
+            .required()
+            .messages({
+              "string.base": "Email must be text",
+              "string.empty": "Email cannot be empty",
+              "string.min": "Email must be 3 character at least",
+              "string.email": "Please sign in your email",
+              "string.max": "Email must be 10 character at most",
+              "string.required": "Emailmust be",
+            }),
+          password: joi.string().trim().min(6).max(36).required().messages({
+            "string.base": "Password must be text",
+            "string.empty": "Password cannot be empty",
+            "string.min": "password must be 6 character at least",
+            "string.max": "Password must be 36 character at most",
+            "string.required": "Password must be",
+          }),
+        })
+        .validateAsync(req.body);
     } catch (error) {
       if (error.details && error?.detail[0].message)
         throw new APIError(error.detail[0].message, 400);
       else throw new APIError("please follow the validation rules", 400);
     }
+    next();
   };
 }
 
